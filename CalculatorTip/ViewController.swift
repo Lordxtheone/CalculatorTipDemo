623de0606121ea038ed6fef572ab4ecb0e9ef1ad
Lordxtheone <mduchatelier@me.com>
2020-04-05 08:59:45 -0400

    Initial Commit

---
+++ CalculatorTip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorTip
+//
+//  Created by Marc D. on 4/5/20.
+//  Copyright © 2020 Marc D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
